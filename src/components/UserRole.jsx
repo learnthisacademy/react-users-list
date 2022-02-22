4765f153f6fb8ea8d7f3389470b8cc5387a7f9f2
Pablo <pablocd168@gmail.com>
2022-02-22 16:57:11 +0100

    UserRow state

--- src/components/UserRole.jsx
+++ src/components/UserRole.jsx
@@ -1,12 +1,12 @@
 import style from './UserRole.module.css';
 
-const UserRole = ({ role }) => {
-	const ROLE_STYLES = {
-		teacher: ['Profesor', style.teacher],
-		student: ['Alumno', style.student],
-		other: ['Otro', style.other]
-	};
+const ROLE_STYLES = {
+	teacher: ['Profesor', style.teacher],
+	student: ['Alumno', style.student],
+	other: ['Otro', style.other]
+};
 
+const UserRole = ({ role }) => {
 	const [roleName, roleClassname] = ROLE_STYLES[role] || ROLE_STYLES.other;
 
 	return <span className={`${style.role} ${roleClassname}`}>{roleName}</span>;
--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,19 +1,33 @@
+import { useState } from 'react';
 import UserRole from './UserRole';
 import style from './UserRow.module.css';
 import UserStatus from './UserStatus';
 
-const UserRow = ({ name, active, role }) => (
-	<div className={style.wrapper}>
-		<div className={style.name}>
-			<span>{name}</span>
+const UserRow = ({ name, active, role }) => {
+	const [isActive, setIsActive] = useState(active);
+
+	return (
+		<div className={style.wrapper}>
+			<div className={style.name}>
+				<span>{name}</span>
+			</div>
+			<div className={style.status}>
+				<UserStatus active={isActive} />
+			</div>
+			<div className={style.role}>
+				<UserRole role={role} />
+			</div>
+			<div className={style.action}>
+				<button
+					onClick={() => {
+						setIsActive(!isActive);
+					}}
+				>
+					{isActive ? 'Desactivar' : 'Activar'}
+				</button>
+			</div>
 		</div>
-		<div className={style.status}>
-			<UserStatus active={active} />
-		</div>
-		<div className={style.role}>
-			<UserRole role={role} />
-		</div>
-	</div>
-);
+	);
+};
 
 export default UserRow;
