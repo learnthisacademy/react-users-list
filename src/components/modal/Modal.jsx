1b61ee7582cf7b7765b25988273ea1ef25a1e789
Pablo <pablocd168@gmail.com>
2022-08-30 11:00:57 +0200

    Modal scroll

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import IconButton from '../buttons/IconButton';
 import CrossIcon from '../icons/CrossIcon';
 import style from './Modal.module.css';
 
 const Modal = ({ closeModal, children }) => {
+	useEffect(() => {
+		if (!children) return;
+
+		document.body.classList.add(style.bodyOverflow);
+
+		return () => {
+			document.body.classList.remove(style.bodyOverflow);
+		};
+	}, [children]);
+
 	if (!children) return null;
 
 	return createPortal(
